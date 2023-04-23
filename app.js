176823b2498fb1a4da92e792de5d41966a228ee9
Katarzyna Śliwka <katarzynasliwka@Katarzynas-MacBook-Pro.local>
2023-04-23 21:19:26 +0200

    new port

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const cart = require("./routes/cart");
 
 const MongoDBStore = require('connect-mongodb-session')(session);
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/ecommerce_plant";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27018/";
 mongoose.connect(dbUrl); //gdzie znajduje się nasza db (wpisując use db to zamiast db wpisuje plant-shop)
 
 const db = mongoose.connection;
