d772faa5392cb50ec8c71a6e2d00a4dc9a15f681
Katarzyna Śliwka <katarzynasliwka@Katarzynas-MacBook-Pro.local>
2023-04-23 21:38:31 +0200

    changing mongo uri

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const cart = require("./routes/cart");
 
 const MongoDBStore = require('connect-mongodb-session')(session);
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27018/plantCluster";
+const dbUrl = process.env.DB_URL || "mongodb://0.0.0.0:27017/plantCluster";
 mongoose.connect(dbUrl); //gdzie znajduje się nasza db (wpisując use db to zamiast db wpisuje plant-shop)
 
 const db = mongoose.connection;
